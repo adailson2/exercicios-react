62372c81f85503b5e528400734805aafe9118b33
Adailson <adailson2@gmail.com>
2019-07-09 01:13:22 -0300

    Sistema de módulos(Import/Export)

--- src/componentes/DoisComponentes.jsx
+++ src/componentes/DoisComponentes.jsx
@@ -7,3 +7,15 @@ const CompB = props =>
     <h1>Segundo diz: {props.valor}</h1>
 
 export { CompA,CompB }
+export default CompA
+
+// export const CompA = props =>
+//     <h1>Primeiro diz: {props.valor}</h1>
+
+// export const CompB = props =>
+//     <h1>Segundo diz: {props.valor}</h1>
+
+// export default function(){
+//     return <p>teste</p>
+// }
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
-// import { CompA, CompB as B } from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
+import CompA, { CompB as B } from './componentes/DoisComponentes'
+// import MultiElementos from './componentes/MultiElementos'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
   <div>
-    <MultiElementos />
-    {/* <CompA valor="Olá eu sou A!" />
+    {/* <MultiElementos /> */}
+    <CompA valor="Olá eu sou A!" />
     <B valor="Olá eu sou B!" />
-    <PrimeiroComponente valor="Bom dia!"/> */}
+    {/* <PrimeiroComponente valor="Bom dia!"/> */}
   </div>, elemento)
