9f5a3ae5bb7e50c75ca499e46cced221f4d64ada
Adailson <adailson2@gmail.com>
2019-07-09 23:30:29 -0300

    Utilizando Hook para utilizar estados em componentes funcionais

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import ReactDOM from 'react-dom'
 // import ComponenteComFuncao from './componentes/ComponenteComFuncao'
 // import Pai from './componentes/Pai'
 // import ComponenteClasse from './componentes/ComponenteClasse'
-import Contador from './componentes/Contador'
+// import Contador from './componentes/Contador'
+import Hook from './componentes/Hook'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
   <div>
-    <Contador numeroInicial = {100}/>
+    <Hook />
+    {/* <Contador numeroInicial = {100}/> */}
     {/* <ComponenteClasse valor = "Sou um componente de classe"/> */}
     {/* <Pai /> */}
     {/* <ComponenteComFuncao /> */}
